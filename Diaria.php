d23b9577955666a68de8d16bb68418a4100ffbc3
Alexander Fonseca <motoleco@hotmail.com>
2022-06-30 11:35:12 -0300

    classes abstratas

--- Diaria.php
+++ Diaria.php
@@ -1,9 +1,11 @@
 <?php
-
-class Atendimento
+//classe abstrata nunca poderá gerar objetos
+// somente como base para herança
+abstract class Atendimento
 {
   public $data;
   private $tempo;
+  protected $valor;
 
   public function definirTempo($tempo)
   {
@@ -14,9 +16,17 @@ class Atendimento
     $this->tempo = $tempo;
   }
 }
+class Diaria extends Atendimento
+{
+  public function definirValor($valor)
+  {
+    $this->valor = $valor;
+  }
+}
+// $at1 = new Atendimento;
+// $at1->data = '01/10/2022';
+// $at1->definirTempo(8);
+$d1 = new Diaria;
+$d1->definirValor(100.00);
+$d1->data = '01/10/2022';
 
-$at1 = new Atendimento;
-$at1->data = '01/10/2022';
-$at1->definirTempo(0);
-
-var_dump($at1);
